9ce88942e1a6ba4fae38233a5e44c5fad29c8850
Alebecito <66501163+Alebecito@users.noreply.github.com>
2022-08-22 17:02:20 -0600

    Fix memory leak processes

--- Processes/MatrixP.c
+++ Processes/MatrixP.c
@@ -56,7 +56,7 @@ void create_processes(int matrix_size, int (*result)[matrix_size], int **matrixA
             // result[i] = *matrixC[i];
             // printf("\t hola from %d\n", i);
             memcpy(result[i], matrixC[i], matrix_size * sizeof(int));
-
+            free(matrixC[i]);
             exit(0);
         }
         else
@@ -170,6 +170,12 @@ int main()
 
         // printf("Time: %f seconds\n", diff);
         fclose(stats_file);
+        for (int z = 0; z < matrix_size; z++)
+        {
+            free(A[z]);
+            free(B[z]);
+            free(C[z]);
+        }
         free(A), free(B), free(C);
         shmdt(result); // free segment
         shmctl(buff, IPC_RMID, NULL);
@@ -180,59 +186,4 @@ int main()
     // printf("Average time: %f seconds\n", average / 100);
     write_stats(average / 100, "Average:");
     return 0;
-    // allocate memory for the matrices and initialize them with random values
-    // int **A = generate_random_matrix(matrix_size),
-    //     **B = generate_random_matrix(matrix_size),
-    //     **C = generate_random_matrix(matrix_size);
-
-    // // print the results
-    // char filename[12];
-    // sprintf(filename, "Mat_%s%d.txt", 5 < 10 ? "0" : "" , 5);
-    // write_matrix(filename, A, matrix_size);
-    // printf("Matrix A:\n");
-    // print_matrix(A, matrix_size);
-    // printf("Matrix B:\n");
-    // print_matrix(B, matrix_size);
-
-    // // ============= NEW CODE =============
-    // // memory segment (ID)
-    // int buff;
-    // // array to share
-    // int(*result)[matrix_size]; // pointer to array
-
-    // buff = shmget(IPC_PRIVATE, sizeof(int[matrix_size][matrix_size]), IPC_CREAT | 0666);
-    // // result receives the memory segment from buff
-    // result = shmat(buff, 0, 0);
-
-    // clock_gettime(CLOCK_MONOTONIC, &start);
-
-    // create_processes(matrix_size, result, A, B, C);
-
-    // // ============= FINIH =============
-
-    // shmctl(buff, IPC_RMID, NULL); // free segment
-
-    // clock_gettime(CLOCK_MONOTONIC, &end);
-
-    // // printf("Matrix RESULT:\n");
-    // // for (int i = 0; i < matrix_size; i++)
-    // // {
-    // //     for (int j = 0; j < matrix_size; j++)
-    // //     {
-    // //         printf("%d ", result[i][j]);
-    // //     }
-    // //     printf("\n");
-    // // }
-
-    // // printf("RESULT\n");
-    // // print_matrix(new_matrix_result, matrix_size);
-
-    // double diff = (end.tv_sec - start.tv_sec);
-    // diff += (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-
-    // printf("Time: %f seconds\n", diff);
-
-    // while(wait(NULL) > 0) {
-    //     printf("TODAVIA HAY SUBPROCESOS");
-    // }
 }
