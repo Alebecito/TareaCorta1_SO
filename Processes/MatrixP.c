150a81e95ebf6b47054d2ab4ce7afa7c16a69297
Alebecito <66501163+Alebecito@users.noreply.github.com>
2022-09-01 18:43:47 -0600

    Comentarios eliminados

--- Processes/MatrixP.c
+++ Processes/MatrixP.c
@@ -10,15 +10,6 @@
 #include <sys/shm.h>
 #include <time.h>
 
-// void copy_matrix(int **original_matrix, int **copy_matrix, int matrix_size)
-// {
-//     // for (int i = 0; i < rows; i++) {
-//     //     for (int j = 0; j < columns; j++) {
-//     //         copy_matrix[i][j] = original_matrix[i][j];
-//     //     }
-//     // }
-//     memcpy(copy_matrix, original_matrix, matrix_size * matrix_size * sizeof(int));
-// }
 
 void create_processes(int matrix_size, int (*result)[matrix_size], int **matrixA, int **matrixB, int **matrixC)
 {
@@ -30,77 +21,23 @@ void create_processes(int matrix_size, int (*result)[matrix_size], int **matrixA
         pid = fork();
         p_ids[i] = pid;
 
-        // if (pid == 0)
-        // {
-        //     printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
-        //     exit(0);
-        // }
-
-        // if (pid == -1)
-        // {
-        //     printf("Error creating child process\n");
-        //     exit(0);
-        // }
         if (pid == 0) // fork returns 0 to the child process so it enters "if" block
         {
-            // printf("Initianting process for row %d\n", i);
-            // close(p[0]);
-            // printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
             multiply_row_by_matrix(matrixA[i], matrix_size, i, matrixB, matrixC);
-
-            // copy the result to the shared memory segment
-            // for (int j = 0; j < matrix_size; j++)
-            // {
-            //     // just copy the new row to the shared memory segment
-            //     // if not matrixC will have a weird behavior
-            //     result[i][j] += matrixC[i][j];
-            // }
-            // result[i] = *matrixC[i];
-            // printf("\t hola from %d\n", i);
             memcpy(result[i], matrixC[i], matrix_size * sizeof(int));
             free(matrixC[i]);
             exit(0);
         }
-        // else
-        // {
-        //     // printf("ELSE BLOCK\n");
-        //     wait(&pid);
-        // }
-        // printf("=========\n");
-
     }
     if (pid != 0)
     {
         int cpid;
         int status;
         while ((cpid = wait(&status)) > 0)
-            //sleep(0.000001);
-            // printf("Parent (%d): child (%d) exited with status 0x%.4x\n", (int)getpid(), cpid, status);
-            // printf("");
+            sleep(0.000001);
             ;
         ;
     }
-    // printf("*************** PROCESSES FINISHED ****************\n");
-    // printf("============================\n");
-    // fputs("JUPON\n", stats_file);
-
-    // copy_matrix(matrixC, (int **)result, matrix_size);
-
-    // printf("Matrix RESULT:\n");
-    // for (int i = 0; i < matrix_size; i++)
-    // {
-    //     for (int j = 0; j < matrix_size; j++)
-    //     {
-    //         printf("%d ", result[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
-    // return (int **)result;
-    // return matrixC;
-
-    // printf("Matrix C:\n");
-    // print_matrix(matrixC, matrix_size);
 }
 
 struct timespec start, end;
@@ -127,11 +64,6 @@ int main()
             **B = generate_random_matrix(matrix_size),
             **C = generate_random_matrix(matrix_size);
 
-        // print the results
-        // printf("Matrix A:\n");
-        // print_matrix(A, matrix_size);
-        // printf("Matrix B:\n");
-        // print_matrix(B, matrix_size);
 
         // ============= NEW CODE =============
         // memory segment (ID)
@@ -139,7 +71,7 @@ int main()
         // array to share
         int(*result)[matrix_size]; // pointer to array
 
-        buff = shmget(IPC_PRIVATE, sizeof(int[matrix_size][matrix_size]), IPC_CREAT | 0666);
+        buff = shmget(IPC_PRIVATE, sizeof(int[matrix_size][matrix_size]), IPC_CREAT);
         // result receives the memory segment from buff
         result = shmat(buff, 0, 0);
 
@@ -150,16 +82,6 @@ int main()
         // ============= FINIH =============
 
         clock_gettime(CLOCK_MONOTONIC, &end);
-
-        // printf("Matrix RESULT:\n");
-        // for (int i = 0; i < matrix_size; i++)
-        // {
-        //     for (int j = 0; j < matrix_size; j++)
-        //     {
-        //         printf("%d ", result[i][j]);
-        //     }
-        //     printf("\n");
-        // }
 
         char filename[12];
         sprintf(filename, "Mat_%s%d.txt", i < 10 ? "0" : "", i);
@@ -173,10 +95,6 @@ int main()
         printf("%s%f\n", iteration, diff);
 
         write_stats(diff, iteration);
-        // fprintf(stats_file, "JUPON\n");
-        // fputs("JUPON\n", stats_file);
-
-        // printf("diff in %f: %d\n", i, diff);
         average += diff;
 
         // printf("Time: %f seconds\n", diff);
